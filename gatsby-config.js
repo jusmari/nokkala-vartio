753572bfc37cc2cdff6894cca40e5045fd28279c
Jussi Maristo <jussi.maristo@perfektio.fi>
2020-03-30 00:53:34 +0300

    changed desc.  (test)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Nokkalan vartiokalenteri`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Kalenteri Nokkalan venesataman vartiovuoroille`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/calendar-navigator.js
+++ src/components/calendar-navigator.js
@@ -1,20 +1,18 @@
 import React from "react"
 
-const CalendarNavigator = ({ handleChange, handleReset }) => {
-  return (
-    <div style={styles.container} className="grid">
-      <button style={styles.button} onClick={() => handleChange(-1)}>
-        Edellinen
-      </button>
-      <button style={styles.button} onClick={() => handleReset()}>
-        Tänään
-      </button>
-      <button style={styles.button} onClick={() => handleChange(1)}>
-        Seuraava
-      </button>
-    </div>
-  )
-}
+const CalendarNavigator = ({ handleChange, handleReset }) => (
+  <div style={styles.container} className="grid">
+    <button style={styles.button} onClick={() => handleChange(-1)}>
+      Edellinen
+    </button>
+    <button style={styles.button} onClick={() => handleReset()}>
+      Tänään
+    </button>
+    <button style={styles.button} onClick={() => handleChange(1)}>
+      Seuraava
+    </button>
+  </div>
+)
 
 const styles = {
   container: {
